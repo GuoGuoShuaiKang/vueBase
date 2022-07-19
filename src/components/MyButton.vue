a7c982b9d6335061b64993adf4f5b21cb15f0d0b
guoshuaikang <1029895026@qq.com>
2022-07-19 17:17:20 +0800

    初始化vue仓库

---
+++ src/components/MyButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>{{msg}}</h2>
+        <button @click="count++">砸了{{count}}次</button>
+    </div>
+</template>
+<script>
+    export default{
+        name:'',
+        data() {
+            return {
+                msg:'铁锤妹妹~',
+                count:0
+            }
+        }
+    }
+</script>
+<style scoped>
+    h2{
+        color: hotpink;
+    }
+    button{
+        color: aqua;
+    }
+</style>
